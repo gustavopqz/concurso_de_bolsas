3baf7ef49a2417e05e67b8efab0b4bfc975d4d07
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-01-29 13:15:21 -0300

    Concluída a consulta por primeiro nome, nome e sebrenome ou nome completo. N

--- index.py
+++ index.py
@@ -6,61 +6,51 @@ df = pd.read_csv('resultado.csv',delimiter= ';')
 inputNome = st.text_input('Digite o nome do candidato:').replace(' ','').lower()
 
 nomes = df['Nome candidato']
+def info(conteudo):
+    for elem in conteudo:
+        return elem
+
+def getInfo(*args):
+    tuple = args
+    candidatoSelecionado = ''.join(tuple)
+    candidato = df.loc[df['Nome candidato'] == candidatoSelecionado]
+
+
+    idAvaliacao = info(candidato['ID avaliação'])
+    avaliacao = info(candidato['Avaliação'])
+    finalizacao = info(candidato['Finalização'])
+    codCandidato = info(candidato['Cód. cand.'])
+    nomeCandidato = info(candidato['Nome candidato'])
+    curso = info(candidato['Inscrição curso'])
+    dataEnvio = info(candidato['Enviada'])
+    situacao = info(candidato['Situação'])
+    nota = info(candidato['Nota'])
+    notaMinima = info(candidato['Nota Mínima'])
+    avaliador = info(candidato['Avaliador'])
+    dataCorrecao = info(candidato['Data correção'])
+
+    infoCandidato = {
+        'Nome do candidato': nomeCandidato,
+        'Código do candidato': codCandidato,
+        'ID Avaliação': idAvaliacao,
+        'Avaliação': avaliacao,
+        'Curso': curso,
+        'Nota': nota,
+        'Nota mínima': notaMinima,
+        'Data de envio': dataEnvio,
+        'Data de correção': dataCorrecao,
+        'Avaliador': avaliador,
+        'Situação': situacao,
+        'Finalização': finalizacao
+    }
+
+    st.write(infoCandidato)
 
 for nome in nomes:
     separado = nome.lower().split(' ')
     if nome.replace(' ','').lower() == inputNome:
-        def getName():
-            candidato = df.loc[df['Nome candidato'] == nome]
-            idAvaliacao = candidato['ID avaliação']
-            avaliacao = candidato['Avaliação']
-            finalizacao = candidato['Finalização']
-            codCandidato = candidato['Cód. cand.']
-            nomeCadidato = candidato['Nome candidato']
-            curso = candidato['Inscrição curso']
-            dataEnvio = candidato['Enviada']
-            situacao = candidato['Situação']
-            nota = candidato['Nota']
-            notaMinima = candidato['Nota Mínima']
-            avaliador = candidato['Avaliador']
-            dataCorrecao = candidato['Data correção']
-            for column in candidato:
-                st.markdown(f'{candidato[column]} ')
-
-        st.button(nome, on_click=getName)
-        
+        st.button(nome, on_click=getInfo, args=nome)
     elif separado[0] == inputNome or separado[1] == inputNome:
-        st.button(nome)
+        st.button(nome, on_click=getInfo, args=nome)
     elif separado[0] + separado[1] == inputNome:
-        st.button(nome)
-
-# maria = df.loc[df['Nome candidato'] == 'Maria Eloiza Oliveira Ferreira ']
-# idAvaliacao = maria['ID avaliação']
-# avaliacao = maria['Avaliação']
-# finalizacao = maria['Finalização']
-# codCandidato = maria['Cód. cand.']
-# nomeCadidato = maria['Nome candidato']
-# curso = maria['Inscrição curso']
-# dataEnvio = maria['Enviada']
-# situacao = maria['Situação']
-# nota = maria['Nota']
-# notaMinima = maria['Nota Mínima']
-# avaliador = maria['Avaliador']
-# dataCorrecao = maria['Data correção']
-
-# for column in df:
-#     print(df[column][0])
-
-# def getName():
-#     nomeCadidato = df.loc[df['Nome candidato']]
-
-# if inputNome:
-#     candidato = df.loc[df['Nome candidato'] == inputNome]
-#     st.button(inputNome, on_click=getName)
-
-#     for column in candidato:
-#         st.markdown(f'{candidato[column].name} - {candidato[column][0]}')
-
-
-
-
+        st.button(nome, on_click=getInfo, args=nome)
