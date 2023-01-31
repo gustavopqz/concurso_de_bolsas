170e6798db8ebba96372415ab720374427416c47
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-01-31 13:08:31 -0300

    Iniciado alterações para xlsx oficial

--- index.py
+++ index.py
@@ -1,16 +1,14 @@
 import streamlit as st
 import pandas as pd
+import random
 
 st.title('Concurso de bolsas 2023.1')
 
-df = pd.read_csv('resultado.csv',delimiter= ';')
+df = pd.read_excel('CDB UNIFAN 2023.1.xlsx')
 
 inputNome = st.text_input('Digite o nome do candidato:').replace(' ','').lower()
 
 nomes = df['Nome candidato']
-def info(conteudo):
-    for elem in conteudo:
-        return elem
 
 infoCandidato = {}
 
@@ -20,32 +18,10 @@ def getInfo(*args):
     candidato = df.loc[df['Nome candidato'] == candidatoSelecionado]
 
 
-    idAvaliacao = info(candidato['ID avaliação'])
-    avaliacao = info(candidato['Avaliação'])
-    finalizacao = info(candidato['Finalização'])
-    codCandidato = info(candidato['Cód. cand.'])
-    nomeCandidato = info(candidato['Nome candidato'])
-    curso = info(candidato['Inscrição curso'])
-    dataEnvio = info(candidato['Enviada'])
-    situacao = info(candidato['Situação'])
-    nota = info(candidato['Nota'])
-    notaMinima = info(candidato['Nota Mínima'])
-    avaliador = info(candidato['Avaliador'])
-    dataCorrecao = info(candidato['Data correção'])
+    nomeCandidato = candidato['Nome candidato']
  
     infoCandidato = {
-        'Nome do candidato': [nomeCandidato],
-        'Código do candidato': [codCandidato],
-        'ID Avaliação': [idAvaliacao],
-        'Avaliação': [avaliacao],
-        'Curso': [curso],
-        'Nota': [nota],
-        'Nota mínima': [notaMinima],
-        'Data de envio': [dataEnvio],
-        'Data de correção': [dataCorrecao],
-        'Avaliador': [avaliador],
-        'Situação': [situacao],
-        'Finalização': [finalizacao]
+        'Nome do candidato': [nomeCandidato]
     }
     st.title(nomeCandidato)
     st.table(pd.DataFrame.from_dict(infoCandidato, orient='index'))
@@ -53,9 +29,9 @@ def getInfo(*args):
 for nome in nomes:
     separado = nome.lower().split(' ')
     if nome.replace(' ','').lower() == inputNome:
-        st.button(nome, on_click=getInfo, args=nome)
+        st.button(nome, on_click=getInfo, args=nome, key=f'{random.random()}')
     elif separado[0] == inputNome or separado[1] == inputNome:
-        st.button(nome, on_click=getInfo, args=nome)
+        st.button(nome, on_click=getInfo, args=nome, key=f'{random.random()}')
     elif separado[0] + separado[1] == inputNome:
-        st.button(nome, on_click=getInfo, args=nome)
+        st.button(nome, on_click=getInfo, args=nome, key=f'{random.random()}')
     
